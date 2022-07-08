7cebfb7db5a1fd4f6fc6fcf570797a105bb1c9e2
Sven Jäger <sven.jaeger@optitool.de>
2022-07-08 14:44:08 +0200

    use vcpkg for dependencies

---
+++ installvcpkg.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+vcpkgPath=~/vcpkg
+
+
+cd $vcpkgPath
+
+#init vcpkg
+./bootstrap-vcpkg.sh
+
+#build linux dependenciess
+./vcpkg install --recurse --clean-after-build gdal[core] geos boost-log boost-program-options shapelib libosmium
