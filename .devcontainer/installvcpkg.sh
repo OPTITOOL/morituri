676cb234bebe6c0b4ddcd265a723cf17158aba01
Sven Jäger <sven.jaeger@optitool.de>
2023-03-01 08:56:15 +0000

    use devcontainer

---
+++ .devcontainer/installvcpkg.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+vcpkgPath=~/vcpkg
+
+cd ~
+
+####### vcpkg
+rm -rf $vcpkgPath
+git clone -b fix-shapelib --depth 1 https://github.com/OPTITOOL/vcpkg.git
+cd $vcpkgPath
+
+#init vcpkg
+./bootstrap-vcpkg.sh
+
+#build linux dependenciess
+./vcpkg install --clean-after-build gdal[core] geos boost-log boost-program-options boost-locale boost-timer boost-iostreams shapelib libosmium
