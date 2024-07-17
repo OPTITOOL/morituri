0333ebf1fab3d3bec5d4791741f8a9b76af07090
Sven Jäger <sven.jaeger@optitool.de>
2024-07-17 12:49:10 +0000

    add country codes

--- plugins/navteq/navteq_plugin.hpp
+++ plugins/navteq/navteq_plugin.hpp
@@ -44,8 +44,62 @@ private:
   bool debug;
 
   const std::map<std::string, std::string> HERE_REGION_ISO_MAPPING = {
-      {"M2", "SLO"}, {"M3", "ALB"}, {"M4", "MKD"}, {"M5", "SRB"},
-      {"M6", "BIH"}, {"M7", "BLR"}, {"M9", "MLT"}};
+      {"A1", "AUT"},                // Austria
+      {"A9", "GRC"}, {"GK", "GRC"}, // Greece
+      {"T1", "TUR"}, {"T3", "TUR"}, {"T4", "TUR"}, {"T5", "TUR"},
+      {"T6", "TUR"}, {"T7", "TUR"}, // Turkey
+      {"AB", "AZE"},                // Azerbaijan
+      {"BB", "BEL"},                // Belgium
+      {"C1", "CZE"}, {"C4", "CZE"}, // Czechia
+      {"CA", "CHE"},                // Switzerland
+      {"E7", "IRL"},                // Ireland
+      {"E1", "GBR"}, {"E2", "GBR"}, {"E3", "GBR"}, {"E4", "GBR"},
+      {"E5", "GBR"}, {"E6", "GBR"}, // Great britain
+      {"F1", "FRA"}, {"F2", "FRA"}, {"F3", "FRA"}, {"F4", "FRA"},
+      {"F5", "FRA"}, {"F6", "FRA"},                               // France
+      {"SP", "FIN"}, {"SQ", "FIN"}, {"SU", "FIN"}, {"SV", "FIN"}, // Finland
+      {"G1", "DEU"}, {"G2", "DEU"}, {"G3", "DEU"}, {"G4", "DEU"},
+      {"G5", "DEU"}, {"G6", "DEU"}, {"G7", "DEU"}, {"G8", "DEU"}, // Germany
+      {"H1", "HUN"},                                              // Hungary
+      {"H2", "HRV"},                                              // Croatia
+      {"H3", "SVN"},                                              // Slovenia
+      {"H4", "EST"},                                              // Estonia
+      {"H5", "LVA"},                                              // Latvia
+      {"H6", "LTU"},                                              // Lithuania
+      {"H7", "BGR"},                                              // Bulgaria
+      {"H8", "ROU"},                                              // Rumania
+      {"H9", "MDA"},                                              // Moldova
+      {"I1", "ITA"}, {"I2", "ITA"}, {"I3", "ITA"}, {"I4", "ITA"},
+      {"I5", "ITA"},                               // Italy
+      {"K4", "PRT"}, {"K6", "PRT"}, {"K8", "PRT"}, // Portugal
+      {"K1", "ESP"}, {"K2", "ESP"}, {"K3", "ESP"}, {"K5", "ESP"},
+      {"K7", "ESP"}, {"K9", "ESP"},                // Spain
+      {"KT", "SRB"},                               // Kosovo
+      {"C5", "CYP"}, {"KQ", "CYP"},                // cyprus
+      {"LU", "LUX"},                               // Luxembourg
+      {"MK", "MNE"},                               // Montenegro
+      {"M2", "SVK"},                               // Slovakia
+      {"M3", "ALB"},                               // Albania
+      {"M4", "MKD"},                               // Macedonia
+      {"M5", "SRB"},                               // Serbia
+      {"M6", "BIH"},                               // Bosnia and Herzegovina
+      {"M7", "BLR"},                               // Belarus
+      {"M9", "MLT"},                               // Malta
+      {"N1", "NOR"},                               // Norway
+      {"N2", "ISL"},                               // Iceland
+      {"N7", "NLD"}, {"NL", "NLD"},                // Netherland
+      {"P1", "POL"}, {"P2", "POL"}, {"P3", "POL"}, // Poland
+      {"S1", "SWE"}, {"S2", "SWE"}, {"S3", "SWE"}, // Sweden
+      {"S4", "DNK"},                               // Denmark
+      {"U1", "UKR"}, {"U2", "UKR"}, {"U3", "UKR"}, {"U4", "UKR"},
+      {"U6", "UKR"}, {"UR", "UKR"}, // Ukraine
+      {"4A", "RUS"}, {"4B", "RUS"}, {"4C", "RUS"}, {"4D", "RUS"},
+      {"4E", "RUS"}, {"4F", "RUS"}, {"4G", "RUS"}, {"4H", "RUS"},
+      {"4J", "RUS"}, {"R1", "RUS"}, {"R2", "RUS"}, {"R3", "RUS"},
+      {"R4", "RUS"}, {"R5", "RUS"}, {"R6", "RUS"}, {"R7", "RUS"},
+      {"R8", "RUS"}, {"R9", "RUS"}, {"RF", "RUS"}, {"RG", "RUS"},
+      {"RH", "RUS"}, {"RK", "RUS"}, {"RZ", "RUS"} // Russia
+  };
 
 public:
   navteq_plugin(const std::filesystem::path &executable_path);
